382be79656eedbb35ef31e1e3a2cb077dc9c77a7
Shubham Bhele <shubham.bhele@qtsolv.com>
2026-06-11 16:37:16 +0530

    chore: sync test framework [2026-06-11T11:07:16.321Z]

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   fullyParallel: Boolean(env.fullyParallel ?? false),
   workers:       Number(env.workers        ?? 1),
   use: {
-    storageState:  ".auth/shubhamESS.json",
+    storageState:  ".auth/essShubh.json",
     baseURL:       String(env.baseURL       ?? "https://example.com"),
     headless:      env.headless !== false,
     trace:         (String(env.trace        ?? "retain-on-failure")) as "off" | "on" | "retain-on-failure" | "on-all-retries",
